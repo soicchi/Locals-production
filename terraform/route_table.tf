d6f109adf1546542f3103a7076cdc58042265e78
soicchi <soicchi@icloud.com>
2022-02-15 14:29:36 +0900

    追加：terraformにてroute_table, internet_gateway作成

---
+++ terraform/route_table.tf
@@ -0,0 +1,40 @@
+# Route Table #
+
+resource "aws_default_route_table" "default_rtb" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "locals-public-route-table"
+  }
+}
+
+# Association #
+
+resource "aws_route_table_association" "rtb_association_1a" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.front_1a.id
+}
+
+resource "aws_route_table_association" "rtb_association_1c" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.front_1c.id
+}
+
+resource "aws_route_table_association" "rtb_association_1a" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.back_1a.id
+}
+
+resource "aws_route_table_association" "rtb_association_1c" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.back_1c.id
+}
--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,37 +1,59 @@
-resource "aws_subnet" "public_1a" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "back_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "locals-public-1a"
+    Name = "locals-front-1a"
   }
 }
 
-resource "aws_subnet" "public_1c" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "front_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "locals-public-1c"
+    Name = "locals-front-1c"
   }
 }
-resource "aws_subnet" "private_1a" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
+
+resource "aws_subnet" "back_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "locals-back-1a"
+  }
+}
+
+resource "aws_subnet" "back_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "locals-back-1c"
+  }
+}
+
+resource "aws_subnet" "db_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "locals-private-1a"
+    Name = "locals-db-1a"
   }
 }
-resource "aws_subnet" "private_1c" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.3.0/24"
+
+resource "aws_subnet" "db_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "locals-private-1c"
+    Name = "locals-db-1c"
   }
 }
