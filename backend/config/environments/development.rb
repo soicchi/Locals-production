ff911e90dd415b53aba4895815be86497f975307
soicchi <soicchi@icloud.com>
2022-02-17 23:54:52 +0900

    追加：backendにてenvironmentのdevelopmentとproductionのファイルに設定を追加

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -63,4 +63,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # devise default URL setting
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # url_forの設定
+  Rails.application.routes.default_url_options[:host] = 'localhost'
+  Rails.application.routes.default_url_options[:port] = 3000
+
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
 end
--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -110,4 +110,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # devise default URL option
+  config.action_mailer.default_url_options = { host: 'local-s-app.com', port: 80 }
+
+  # url_forの設定
+  Rails.application.routes.default_url_options[:host] = 'locals-apps.com'
+  Rails.application.routes.default_url_options[:port] = 3000
+
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
+
+  config.hosts << 'local-s-app.com'
 end
