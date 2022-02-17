50f4491d705db5094fac5423fd45b4a66be03fe9
soicchi <soicchi@icloud.com>
2022-02-17 16:14:43 +0900

    修正：frontendにてeslintを実行

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -32,7 +32,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/axios'
+    '~/plugins/axios',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -46,7 +46,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
