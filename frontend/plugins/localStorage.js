a014895e78bf2208a23f4cb86a162a32f6da714f
soicchi <soicchi@icloud.com>
2022-02-19 14:24:57 +0900

    追加：frontendにてlocalStorage, swiperの設定ファイルをpluginsに作成

---
+++ frontend/plugins/localStorage.js
@@ -0,0 +1,10 @@
+import createPersistedState from 'vuex-persistedstate'
+
+export default ({ store, req, isDev }) => {
+  createPersistedState({
+    key: 'locals',
+    paths: [
+      'user.loggedInUser',
+    ]
+  })(store)
+}
