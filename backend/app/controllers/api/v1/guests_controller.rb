0ccaed2da574be724405f49d59bd7a53186db257
soicchi <soicchi@icloud.com>
2022-03-02 16:33:28 +0900

    修正：backendにてゲストユーザー用のデータを修正

--- backend/app/controllers/api/v1/guests_controller.rb
+++ backend/app/controllers/api/v1/guests_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::GuestsController < ApplicationController
   def create
     random_value = SecureRandom.hex(2)
+    random_pass = SecureRandom.hex(4)
     user_params = {
       name: "guest_#{random_value}",
       email: "guest_#{random_value}@guest.com",
@@ -8,8 +9,8 @@ class Api::V1::GuestsController < ApplicationController
       birth_year: 1992,
       birth_month: 4,
       birth_day: 4,
-      password: 'password',
-      password_confirmation: 'password',
+      password: "#{random_pass}",
+      password_confirmation: "#{random_pass}",
       guest: true
     }
     user = User.create!(user_params)
