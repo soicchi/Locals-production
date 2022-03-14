406261472bfcfb73ae87cd726489cb1e79e82533
soicchi <soicchi@icloud.com>
2022-03-15 00:46:21 +0900

    修正：frontendにてsigninページのデータ構造を修正

--- frontend/pages/auth/signin.vue
+++ frontend/pages/auth/signin.vue
@@ -1,3 +1,67 @@
 <template>
-  <OrganismsSignIn />
+  <OrganismsSignIn
+    :is-valid.sync="isValid"
+    :loading="loading"
+    :user="user"
+    :card-width="cardWidth"
+    @sign-in="signIn"
+    @guest-sign-in="guestSignIn"
+  />
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data: () => ({
+    isValid: false,
+    loading: false,
+    user: {
+      email: '',
+      password: '',
+    },
+  }),
+  computed: {
+    cardWidth () {
+      return this.$vuetify.breakpoint.xs ? '90%' : '25%'
+    }
+  },
+  methods: {
+    ...mapActions({
+      setLoggedInUser: 'user/setLoggedInUser',
+      setMessages: 'message/setMessages',
+    }),
+    async signIn () {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 3000)
+      await this.$auth.loginWith('local', { data: this.user })
+        .then((res) => {
+          this.setLoggedInUser(res.data.data)
+          const message = ['ログインしました']
+          this.setMessages({ messages: message, type: 'success' })
+        })
+        .catch((e) => {
+          const messages = e.response.data.errors
+          this.setMessages({ messages, type: 'error' })
+        })
+    },
+    async guestSignIn () {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 3000);
+      await this.$axios.post('/guests')
+        .then((res) => {
+          this.$auth.loginWith('local', { data: res.data })
+            .then((res) => {
+              this.setLoggedInUser(res.data.data)
+              const message = ['ログインしました']
+              this.setMessages({ messages: message, type: 'success' })
+            })
+        })
+    },
+  },
+}
+</script>
