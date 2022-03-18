df6573718928ad1dc1271604f96282d330bd70f0
soicchi <soicchi@icloud.com>
2022-03-18 19:22:44 +0900

    本番環境へpush

---
+++ frontend/components/organisms/PostCircleChart.vue
@@ -0,0 +1,34 @@
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  data: () => ({
+    favoriteData: {
+      labels: ['〜10代', '20代', '30代', '40代', '50代', '60代〜'],
+      datasets: [{
+        label: 'いいね割合',
+        data: [12, 11, 35, 2, 1, 39], // サンプルデータ
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+        ],
+        borderWidth: 1,
+      }],
+    },
+  }),
+  mounted () {
+    this.renderChart(this.favoriteData, this.chartOptions)
+  },
+}
+</script>
