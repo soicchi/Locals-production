607ceac8d8752244f27d62ea7b9d1a6cc181f15b
soicchi <soicchi@icloud.com>
2022-03-18 10:32:49 +0900

    修正：frontendにてブックマークページのデータ構造を修正

--- frontend/pages/posts/bookmark.vue
+++ frontend/pages/posts/bookmark.vue
@@ -1,13 +1,32 @@
 <template>
-  <TemplatesBookMarkPage :title="title" />
+  <TemplatesBookMarkPage
+    :title="title"
+    :icon-size="iconSize"
+    :book-mark-posts="bookMarkPosts"
+    :logged-in-user="loggedInUser"
+    :post-card-width="postCardWidth"
+  />
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   computed: {
-    title () {
-      return 'ブックマーク'
+    ...mapGetters({
+      bookMarkPosts: 'bookMark/bookMarks',
+      loggedInUser: 'user/loggedInUser',
+    }),
+    postCardWidth () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return '100%'
+        case 'sm': return '80%'
+        case 'md': return '80%'
+      }
+      return '45%'
     },
+    title: () => 'ブックマーク',
+    iconSize: () => 48,
   },
   created () {
     this.$store.dispatch('bookMark/getBookMarks')
